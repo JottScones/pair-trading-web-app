5f65029ac4397860c8b3f274738d8fa23a832854
scottjones <sj6018@ic.ac.uk>
2022-05-20 22:04:07 +0100

    removed superfluous import that was causing a heroku deployment error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,6 @@ import React from 'react';
 import './App.css';
 import StoredPairs from './storedPairs';
 import {MathJaxContext} from 'better-react-mathjax';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from 'react-router-dom';
 
 class App extends React.Component {
   render() {
