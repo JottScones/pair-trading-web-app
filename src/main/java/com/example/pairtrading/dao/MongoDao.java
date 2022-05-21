0aac90df369ee4b201ba1c8201229f231e34c241
scottjones <sj6018@ic.ac.uk>
2022-05-21 16:20:54 +0100

    Implemented StockDoa methods for MongoDao using queries from the StockRepository interface.

--- src/main/java/com/example/pairtrading/dao/MongoDao.java
+++ src/main/java/com/example/pairtrading/dao/MongoDao.java
@@ -1,17 +1,30 @@
 package com.example.pairtrading.dao;
 
+import com.example.pairtrading.datastore.StockRepository;
 import com.example.pairtrading.model.Stock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@Repository("mongo")
 public class MongoDao implements StockDao{
+
+    @Autowired
+    StockRepository stockRepository;
+
     @Override
     public Optional<Stock> getStockWithTicker(String ticker) {
-        return Optional.empty();
+        return Optional.of(stockRepository.findStockByTicker(ticker));
     }
 
     @Override
     public String[] getAllStocks() {
-        return new String[0];
+        List<Stock> stocks   = stockRepository.findAll();
+        Stream<String> tickers =  stocks.stream().map(Stock::getTickerSymbol);
+        return tickers.toArray(String[]::new);
     }
 }
