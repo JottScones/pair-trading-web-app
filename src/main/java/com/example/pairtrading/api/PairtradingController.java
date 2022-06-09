8b39a197be849ca9e2599c5dcf1b4ccfbab1ff65
scottjones <sj6018@ic.ac.uk>
2022-06-09 19:04:29 +0100

    Added javadoc comments for the controller functions - to add clarity on how to use them.

--- src/main/java/com/example/pairtrading/api/PairtradingController.java
+++ src/main/java/com/example/pairtrading/api/PairtradingController.java
@@ -25,6 +25,16 @@ public class PairtradingController {
         this.stockService = stockService;
     }
 
+    /**
+     * This function implements the get functionality at the stored/distance endpoint. It takes two ticker strings as
+     * request parameters and returns the double sum of square distances of their price histories.
+     *
+     * If either is stock is not found in the database, then we return a NOT_FOUND response.
+     *
+     * @param ticker1
+     * @param ticker2
+     * @return distance between the two given stocks (double)
+     */
     @GetMapping("stored/distance")
     public ResponseEntity getDistance(@RequestParam("ticker1") String ticker1, @RequestParam("ticker2") String ticker2) {
         Optional<Stock> stock1 = stockService.getStockWithTicker(ticker1);
@@ -39,6 +49,16 @@ public class PairtradingController {
         return ResponseEntity.ok(StockProcessor.calculateStockDistance(stock1.get(), stock2.get()));
     }
 
+    /**
+     * This function implements the get functionality at the stored/pearson endpoint. It takes two ticker strings as
+     * request parameters and returns the double pearson correlation coefficient of their price histories.
+     *
+     * If either stock is not found in the database, then we return a NOT_FOUND response.
+     *
+     * @param ticker1
+     * @param ticker2
+     * @return pearson correlation between the two given stocks (double)
+     */
     @GetMapping("stored/pearson")
     public ResponseEntity getPearson(@RequestParam("ticker1") String ticker1, @RequestParam("ticker2") String ticker2) {
         Optional<Stock> stock1 = stockService.getStockWithTicker(ticker1);
@@ -53,6 +73,19 @@ public class PairtradingController {
         return ResponseEntity.ok(StockProcessor.calculatePearsonCoefficient(stock1.get(), stock2.get()));
     }
 
+    /**
+     * This function implements the get functionality at the stored/metrics endpoint. It takes two ticker strings as
+     * request parameters and returns a Metric object converted into its json representation. The Metric object
+     * holds information such as the rolling avg, rolling ratio and rolling standard deviations between the two stock
+     * price histories.
+     *
+     * If either stock is not found in the database, then we return a NOT_FOUND response.
+     * If the stored price history is less than 60 days long than we return an UNPROCESSABLE_ENTITY response.
+     *
+     * @param ticker1
+     * @param ticker2
+     * @return json representation of a Metric object
+     */
     @GetMapping("stored/metrics")
     public ResponseEntity getPerformance(@RequestParam("ticker1") String ticker1, @RequestParam("ticker2") String ticker2) {
         Optional<Stock> stock1 = stockService.getStockWithTicker(ticker1);
@@ -76,6 +109,18 @@ public class PairtradingController {
         return ResponseEntity.ok(metric);
     }
 
+    /**
+     * This  function implements the get functionality at the stored/profit endpoint. It takes two ticker strings as
+     * request parameters and returns a double of profit/loss from using the pair trading algorithm with the two stocks
+     * over the period of six months.
+     *
+     * If either stock is not found in the database, then we return a NOT_FOUND response.
+     * If the stored price history is less than 60 days long than we return an UNPROCESSABLE_ENTITY response.
+     *
+     * @param ticker1
+     * @param ticker2
+     * @return profit or loss from pair trading both stocks (double)
+     */
     @GetMapping("stored/profit")
     public ResponseEntity getTradingProfit(@RequestParam("ticker1") String ticker1, @RequestParam("ticker2") String ticker2) {
         Optional<Stock> stock1 = stockService.getStockWithTicker(ticker1);
@@ -99,6 +144,19 @@ public class PairtradingController {
         return ResponseEntity.ok(profit);
     }
 
+    /**
+     * This function implements the get functionality at the stored/trades endpoint. It takes two ticker strings as
+     * request parameters and returns an array of Trade objects. The Trade objects represent a given trade decision
+     * on a certain day and the current remaining budget (which starts at $1K). This endpoint is used to gain deeper
+     * insight of the decisions the pair trading algorithm made when using the two chosen stocks.
+     *
+     * If either stock is not found in the database, then we return a NOT_FOUND response.
+     * If the stored price history is less than 60 days long than we return an UNPROCESSABLE_ENTITY response.
+     *
+     * @param ticker1
+     * @param ticker2
+     * @return array of daily trades (represented as Trade objects)
+     */
     @GetMapping("stored/trades")
     public ResponseEntity getTrades(@RequestParam("ticker1") String ticker1, @RequestParam("ticker2") String ticker2) {
         Optional<Stock> stock1 = stockService.getStockWithTicker(ticker1);
@@ -122,6 +180,16 @@ public class PairtradingController {
         return ResponseEntity.ok(trades);
     }
 
+    /**
+     * This function implements the get functionality at the stored/stock endpoint. It takes a single ticker string as
+     * a request parameter and, if the stock is found in the database, returns a Stock object containing price history
+     * and the most recent stock date.
+     *
+     * If the stock is not found in the database, then we return a NOT_FOUND response.
+     *
+     * @param ticker
+     * @return json representation of a Stock object
+     */
     @GetMapping("stored/stock")
     public ResponseEntity getStock(@RequestParam("ticker") String ticker) {
         Optional<Stock> stock = stockService.getStockWithTicker(ticker);
@@ -135,6 +203,12 @@ public class PairtradingController {
         return ResponseEntity.ok(stock.get());
     }
 
+    /**
+     * This function returns the get functionality at the root of the stored/ endpoint. It takes no request parameters and
+     * return an array of Strings containing all the ticker symbols of the stored stocks in the database.
+     *
+     * @return array of ticker symbol strings
+     */
     @GetMapping("stored")
     public ResponseEntity getAllStocks() {
         return ResponseEntity.ok(stockService.getAllStocks());
